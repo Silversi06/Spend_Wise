fad89b330224080c79d0bb10667132f0e8ff03fd
GoEun <w2219@e-mirim.hs.kr>
2023-11-17 16:01:21 +0900

    월간 지출 관리

---
+++ src/main/java/org/example/Year.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Year extends JPanel {
+    public Year() {
+        Color backgroundColor = Color.decode("#FFE6B7"); // 연한 주황색
+        this.setBackground(backgroundColor); // 배경 색상 설정
+    }
+}
